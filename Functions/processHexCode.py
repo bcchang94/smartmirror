2c06d00570dfdb30c0c4eb18a6f34f2e7a72b269
Brandon Chang <bcchang94@gmail.com>
2021-03-28 13:09:59 -0700

    Trying ideas for processing IDs and hex chars

--- Functions/processHexCode.py
+++ Functions/processHexCode.py
@@ -9,7 +9,22 @@ if os.path.isdir('Functions/in_SortedHexCode') == False:
 else:
     with open('sortHexCode.csv', 'w') as outFile:
         outFile.write('OpenWeather ID, Day Hex Code, Night Hex Code\n') #add headers to file
-        for item in os.listdir('Functions/in_SortHexCode'):
-            with open('Functions/in_SortHexCode' +item, 'r') as inFile:
-                print('Processing' + item)
-                line_
+        for item in os.listdir('Functions/in_SortedHexCode'):
+            with open('Functions/in_SortedHexCode/' +item, 'r') as inFile:
+                print('Processing ' + item)
+                #line_A = inFile.readline()
+                #line_A = inFile.readline()
+                #openWeatherID_A = line_A[0][-3:]
+                #hexCodeChar_A = line_A[1]
+                #line_B = inFile.readline()
+                #openWeatherID_B = line_B[0][-3:]
+                #hexCodeChar_B = line_B[1]
+                row_count = len(list(inFile)) #determine number of rows in csv file
+                List = [None] * (row_count-1) #-1 to exclude header
+                line = inFile.readline()
+                print(line)
+                for row in range(row_count-1):
+                    List[row] = line
+                    
+                    line = inFile.readline()
+                    #print(List[0])
