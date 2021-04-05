1d408a18e1efc12e11a451a914e1b0f5d6bb618f
Brandon Chang <bcchang94@gmail.com>
2021-04-04 22:54:28 -0700

    Recieve weather API response from API call

---
+++ Functions/getWeather.py
@@ -0,0 +1,28 @@
+'''
+Author: Brandon Chang
+'''
+
+import requests, json
+
+#API Key via OpenWeatherMap
+api_key = 'bc78c5a89ad74cd2b1f85071d5cf357d'
+
+#base URL for API calls
+base_URL = 'https://api.openweathermap.org/data/2.5/onecall?'
+
+#provide lat & long coordinates for city of interest
+lat = '33.4936'
+lon = '117.1484'
+
+#provide city name
+city_name = 'Temecula, CA'
+
+#desired excluded information from API request
+exclude = 'minutely'
+
+complete_URL = base_URL + 'lat=' + lat + '&lon=' + lon + '&units=imperial' + '&exclude=' + exclude + '&appid=' + api_key
+
+api_response = requests.get(complete_URL)
+
+format_api_response = api_response.json()
+print(format_api_response)
