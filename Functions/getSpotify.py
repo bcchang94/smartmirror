b7b059897071a52f9af381367332cd2d47244777
Brandon Chang <bcchang94@gmail.com>
2021-05-28 14:48:25 -0700

    Remove album cover url from return, download image in working directory

--- Functions/getSpotify.py
+++ Functions/getSpotify.py
@@ -3,7 +3,7 @@ Author: Brandon Chang
 Purpose: This function retreives 'Now Playing' information from Spotify
 '''
 
-import os, json, spotipy
+import os, json, spotipy, requests
 from spotipy.oauth2 import SpotifyOAuth
 
 def getSpotify():
@@ -31,9 +31,13 @@ def getSpotify():
     current_track = spotipy.client.Spotify(auth=access_token).currently_playing()
     #print(current_track)
 
+    img_data = requests.get(current_track["item"]["album"]["images"][1]['url']).content
+    with open('album_cover.jpg', 'wb') as handler:
+            handler.write(img_data)
+
     #create dictionary for function return
     return_dict = {
-        "album_cover"   : current_track["item"]["album"]["images"][1],
+        #"album_cover"   : current_track["item"]["album"]["images"][1],
         "album_name"    : current_track["item"]["album"]["name"],
         "artist_name"   : current_track["item"]["artists"][0]["name"],
         "track_name"    : current_track["item"]["name"]
