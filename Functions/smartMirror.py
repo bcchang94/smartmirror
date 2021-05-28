9219f485561e6d9c1dc46a6df83a82f0e8b840b9
Brandon Chang <bcchang94@gmail.com>
2021-05-28 16:35:44 -0700

    Display Spotify currently playing information

--- Functions/smartMirror.py
+++ Functions/smartMirror.py
@@ -4,25 +4,26 @@ Purpose: Main function to display information for smart mirror
 '''
 
 import tkinter as tk
+from PIL import ImageTk, Image
 from getDate import getDate
 from getTime import getTime
 from getWeather import getWeather
 from getSpotify import getSpotify
 
-def greeting_frame():
+def greeting_frame(parent):
     time = getTime()
-    display_time = tk.Label(frame_top, text = time[0])
+    display_time = tk.Label(parent, text = time[0])
     display_time.pack()
-    display_greeting = tk.Label(frame_top, text = time[1])
+    display_greeting = tk.Label(parent, text = time[1])
     display_greeting.pack()
     
     date = getDate()
-    display_date = tk.Label(frame_top, text = '\n' + date)
+    display_date = tk.Label(parent, text = '\n' + date)
     display_date.pack()
 
-def current_weather_frame():
+def current_weather_frame(parent):
     # Frame to hold current weather information packed to the top of bottom left frame
-    frame_weather_current = tk.Frame(frame_left)
+    frame_weather_current = tk.Frame(parent)
     frame_weather_current.pack(fill = 'x')
 
     frame_current_display = tk.Frame(frame_weather_current)
@@ -49,9 +50,9 @@ def current_weather_frame():
     display_current_hum = tk.Label(frame_current_display, text = 'Hum: ' + str(weather_dict['current']['current_humidity']) + '%')
     display_current_hum.pack(padx = 10)
 
-def forecast_weather_frame():
+def forecast_weather_frame(parent):
     # Frame to hold forecast weather information packed below current weather information
-    frame_weather_forecast = tk.Frame(frame_left)
+    frame_weather_forecast = tk.Frame(parent)
     frame_weather_forecast.pack(pady = 30)
 
     for i in range(5):
@@ -76,6 +77,30 @@ def forecast_weather_frame():
         display_pop = tk.Label(frame_forecast_display, text = 'Pop: ' + str(weather_dict['forecast'][i]['day_pop']) + '%')
         display_pop.pack(padx = 10)
 
+def spotify_track_frame(parent):
+    frame_artist_display = tk.Frame(parent)
+    frame_artist_display.pack(fill = 'x')
+
+    frame_track_display = tk.Frame(frame_artist_display)
+    frame_track_display.pack()
+
+    display_track = tk.Label(frame_track_display, text = spotify_dict['track_name'])
+    display_track.pack()
+
+    display_album = tk.Label(frame_track_display, text = spotify_dict['album_name'])
+    display_album.pack()
+
+    display_artist = tk.Label(frame_track_display, text = spotify_dict['artist_name'])
+    display_artist.pack()
+
+    frame_album_display = tk.Frame(parent)
+    frame_album_display.pack()
+    path = 'album_cover.jpg'
+    img = ImageTk.PhotoImage(Image.open(path))
+    display_album_image = tk.Label(frame_album_display, image = img)
+    display_album_image.image = img
+    display_album_image.pack(side = 'bottom', fill = 'both', expand = 'yes')
+
 window = tk.Tk()
 #window.attributes('-fullscreen', True)
 window.geometry('720x900')
@@ -99,8 +124,12 @@ frame_right.pack(side = 'right')
 # Retrieve weather API information
 weather_dict = getWeather()
 
-greeting_frame()
-current_weather_frame()
-forecast_weather_frame()
+# Retreive Spotify API infomration
+spotify_dict = getSpotify()
+
+greeting_frame(frame_top)
+current_weather_frame(frame_left)
+forecast_weather_frame(frame_left)
+spotify_track_frame(frame_right)
 
 window.mainloop()
